c1573bdac5bc74f194d109b23877fc5fe1750e67
Nikita_Ko <nikitaplay1000@gmail.com>
2020-07-06 12:29:55 +0400

    Add platfom

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
+from random import randrange as rnd
 
 WIDTH, HEIGHT = 1200, 800
 fps = 60
+platform_width = 330
+platform_height = 35
+platform_speed = 15
+platform = pygame.Rect(WIDTH // 2 - platform_width // 2, HEIGHT - platform_height - 10, platform_width, platform_height)
 
 pygame.init()
 
@@ -14,6 +19,7 @@ while True:
         if event.type == pygame.QUIT:
             exit()
     game.blit(img, (0, 0))
+    pygame.draw.rect(game, pygame.Color('darkblue'), platform)
 
     pygame.display.flip()
     clock.tick(fps)

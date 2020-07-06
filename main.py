c265f6817f7c5d29d4918556f1b5fd562a38c1a6
Nikita_Ko <nikitaplay1000@gmail.com>
2020-07-06 12:13:59 +0400

    Add a background and img/ directory

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import pygame
 
-WIDTH, HEIGHT = 1280, 720
+WIDTH, HEIGHT = 1200, 800
 fps = 60
 
 pygame.init()
 
 game = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+img = pygame.image.load('img/bg.png').convert()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+    game.blit(img, (0, 0))
 
     pygame.display.flip()
     clock.tick(fps)
